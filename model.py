156f234d58e5dd070ace5174af7c4a6d6999e46a
Kaito-Kido <maidaialam2001@gmail.com>
2022-04-22 08:34:05 +0700

    edit

--- model.py
+++ model.py
@@ -17,6 +17,8 @@ LOG_SIG_MIN = -20
 epsilon = 1e-6
 
 # Initialize Policy weights
+
+
 def weights_init_(m):
     if isinstance(m, nn.Linear):
         torch.nn.init.xavier_uniform_(m.weight, gain=1)
@@ -58,7 +60,7 @@ class QNetwork(nn.Module):
 
     def forward(self, state, action):
         xu = torch.cat([state, action], 1)
-        
+
         x1 = F.relu(self.linear1(xu))
         x1 = F.relu(self.linear2(x1))
         x1 = self.linear3(x1)
@@ -71,9 +73,9 @@ class QNetwork(nn.Module):
 
 
 class GaussianPolicy(nn.Module):
-    def __init__(self, num_inputs, num_actions, action_space=None):
+    def __init__(self, num_inputs, num_actions):
         super(GaussianPolicy, self).__init__()
-        
+
         self.linear1 = nn.Linear(num_inputs, 256)
         self.linear2 = nn.Linear(256, 256)
 
@@ -89,15 +91,18 @@ class GaussianPolicy(nn.Module):
         self.novel = None
         self.delta_f = None
 
+        for param in self.parameters():
+            param.requires_grad = False
+
         # action rescaling
-        if action_space is None:
-            self.action_scale = torch.tensor(1.)
-            self.action_bias = torch.tensor(0.)
-        else:
-            self.action_scale = torch.FloatTensor(
-                (action_space.high - action_space.low) / 2.)
-            self.action_bias = torch.FloatTensor(
-                (action_space.high + action_space.low) / 2.)
+        # if action_space is None:
+        #     self.action_scale = torch.tensor(1.)
+        #     self.action_bias = torch.tensor(0.)
+        # else:
+        #     self.action_scale = torch.FloatTensor(
+        #         (action_space.high - action_space.low) / 2.)
+        #     self.action_bias = torch.FloatTensor(
+        #         (action_space.high + action_space.low) / 2.)
 
     def forward(self, state):
         x = F.relu(self.linear1(state))
@@ -130,6 +135,24 @@ class GaussianPolicy(nn.Module):
         state = torch.FloatTensor(state.reshape(1, -1)).to(device)
         action, _, _ = self.sample(state)
         return action.cpu().data.numpy()[0]
+
+    def save(self, filename):
+        torch.save(self.state_dict(), filename)
+
+    def load(self, filename):
+        self.load_state_dict(torch.load(
+            filename, map_location=torch.device('cpu')))
+
+    def disable_grad(self):
+        for param in self.paramteres():
+            param.requires_grad = False
+
+    def enable_grad(self):
+        for param in self.parameters():
+            param.requires_grad = True
+
+    def return_copy(self):
+        return copy.deepcopy(self)
 
 
 class DeterministicPolicy(nn.Module):
@@ -172,6 +195,7 @@ class DeterministicPolicy(nn.Module):
         self.noise = self.noise.to(device)
         return super(DeterministicPolicy, self).to(device)
 
+
 class Critic(object):
     def __init__(
         self,
@@ -199,24 +223,32 @@ class Critic(object):
 
         self.critic = CriticNetwork(state_dim, action_dim).to(device)
         self.critic_target = copy.deepcopy(self.critic)
-        self.critic_optimizer = torch.optim.Adam(self.critic.paramers(), lr=3e-4)
+        self.critic_optimizer = torch.optim.Adam(
+            self.critic.paramers(), lr=3e-4)
         hard_update(self.critic_target, self.critic)
 
         if self.policy_type == "Gaussian":
             # Target Entropy = −dim(A) (e.g. , -6 for HalfCheetah-v2) as given in the paper
             if self.automatic_entropy_tuning is True:
-                self.target_entropy = -torch.prod(torch.Tensor(action_dim.shape).to(device)).item()
-                self.log_alpha = torch.zeros(1, requires_grad=True, device=device)
+                self.target_entropy = - \
+                    torch.prod(torch.Tensor(
+                        action_dim.shape).to(device)).item()
+                self.log_alpha = torch.zeros(
+                    1, requires_grad=True, device=device)
                 self.alpha_optim = torch.optim.Adam([self.log_alpha], lr=3e-4)
 
-            self.policy = GaussianPolicy(state_dim, action_dim.shape[0], 26, action_dim).to(device)
-            self.policy_optim = torch.optim.Adam(self.policy.parameters(), lr=3e-4)
+            self.policy = GaussianPolicy(
+                state_dim, action_dim.shape[0], 26, action_dim).to(device)
+            self.policy_optim = torch.optim.Adam(
+                self.policy.parameters(), lr=3e-4)
 
         else:
             self.alpha = 0
             self.automatic_entropy_tuning = False
-            self.policy = DeterministicPolicy(state_dim, action_dim.shape[0], 256, action_dim).to(self.device)
-            self.policy_optim = torch.optim.Adam(self.policy.parameters(), lr=3e-4)
+            self.policy = DeterministicPolicy(
+                state_dim, action_dim.shape[0], 256, action_dim).to(self.device)
+            self.policy_optim = torch.optim.Adam(
+                self.policy.parameters(), lr=3e-4)
 
     def train(self, archive, replay_buffer, nr_of_steps, batch_size=256):
         diff = set(archive.keys()) - self.actors_set
@@ -238,22 +270,27 @@ class Critic(object):
         for _ in range(nr_of_steps):
             self.total_it += 1
 
-            state, action, next_state,reward, not_done = replay_buffer.sample(batch_size)
+            state, action, next_state, reward, not_done = replay_buffer.sample(
+                batch_size)
 
             all_target_Q = torch.zeros(batch_size, len(self.actors))
             with torch.no_grad():
                 for idx, actor in enumerate(self.actors):
-                    next_action, next_state_log_pi, _ = self.actor_targets[idx].sample(next_state)
-                    target_Q1, target_Q2 = self.critic_target(next_state, next_action)
-                    target_Q = torch.min(target_Q1, target_Q2) - self.alpha * next_state_log_pi
-                    target_Q[:,idx] = target_Q.squeeze()
-                
+                    next_action, next_state_log_pi, _ = self.actor_targets[idx].sample(
+                        next_state)
+                    target_Q1, target_Q2 = self.critic_target(
+                        next_state, next_action)
+                    target_Q = torch.min(
+                        target_Q1, target_Q2) - self.alpha * next_state_log_pi
+                    target_Q[:, idx] = target_Q.squeeze()
+
                 target_Q = torch.max(all_target_Q, dim=1, keepdim=True)[0]
                 target_Q = reward + not_done * self.gamma * target_Q
 
             current_Q1, current_Q2 = self.critic(state, action)
 
-            critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+            critic_loss = F.mse_loss(
+                current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
 
             self.critic_optimizer.zero_grad()
             critic_loss.backward()
@@ -262,7 +299,7 @@ class Critic(object):
             for idx, actor in enumerate(self.actors):
 
                 pi, log_pi, _ = actor.sample(state)
-                
+
                 qf1_pi, qf2_pi = self.critic(state, pi)
                 min_qf_pi = torch.min(qf1_pi, qf2_pi)
 
@@ -273,7 +310,8 @@ class Critic(object):
                 self.actor_optimisers[idx].step()
 
                 for param, target_param in zip(actor.parameters(), self.actor_targets[idx].parameters()):
-                    target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+                    target_param.data.copy_(
+                        self.tau * param.data + (1 - self.tau) * target_param.data)
 
             # for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
             #     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
@@ -284,11 +322,12 @@ class Critic(object):
         return critic_loss
 
     def save(self, filename):
-		torch.save(self.critic.state_dict(), filename)
-		torch.save(self.critic_optimizer.state_dict(), filename + "_optimizer")
-		
+        torch.save(self.critic.state_dict(), filename)
+        torch.save(self.critic_optimizer.state_dict(), filename + "_optimizer")
 
-	def load(self, filename):
-		self.critic.load_state_dict(torch.load(filename + "_critic", map_location=torch.device('cpu')))
-		self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer", map_location=torch.device('cpu')))
-		self.critic_target = copy.deepcopy(self.critic)
+    def load(self, filename):
+        self.critic.load_state_dict(torch.load(
+            filename + "_critic", map_location=torch.device('cpu')))
+        self.critic_optimizer.load_state_dict(torch.load(
+            filename + "_critic_optimizer", map_location=torch.device('cpu')))
+        self.critic_target = copy.deepcopy(self.critic)
